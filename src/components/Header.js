2ce1618ee7319e0d19ba35d21214eab53ff17f28
Aarati Janardhan <aarati.janardhan@gmail.com>
2024-08-01 16:52:36 +0200

    Modifications to the UI of the portfolio project

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,6 +33,36 @@ const socials = [
 ];
 
 const Header = () => {
+  /**
+   * Scrolling & Animation Effect for the projects section
+   */
+  const headerRef = useRef(null);
+  
+  useEffect(() => {
+    let prevScrollPos = window.scrollY;
+
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      const headerElement = headerRef.current;
+      if (!headerElement) {
+        return;
+      }
+      if (prevScrollPos > currentScrollPos) {
+          headerElement.style.transform = "translateY(0)";
+      } else {
+          headerElement.style.transform = "translateY(-200px)"
+      }
+      prevScrollPos = currentScrollPos;
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+
+  }, [])
+  
   const handleClick = (anchor) => () => {
     const id = `${anchor}-section`;
     const element = document.getElementById(id);
@@ -65,10 +95,30 @@ const Header = () => {
         >
           <nav>
             {/* Add social media links based on the `socials` data */}
+            <HStack >
+              {socials.map(({icon, url})=>(
+                <a
+                key={url}
+                href={url}
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={icon} size="2x" key={url} />
+                </a>
+              ))}
+            </HStack>
           </nav>
           <nav>
             <HStack spacing={8}>
               {/* Add links to Projects and Contact me section */}
+
+              <a href="#projects" onClick={handleClick("projects")}>
+                Projects
+              </a>
+              <a href="#contact-me" onClick={handleClick("contactme")}>
+                Contact Me
+              </a>
+              
             </HStack>
           </nav>
         </HStack>
--- src/hooks/useSubmit.js
+++ src/hooks/useSubmit.js
@@ -13,6 +13,8 @@ const useSubmit = () => {
   const submit = async (url, data) => {
     const random = Math.random();
     setLoading(true);
+   
+   
     try {
       await wait(2000);
       if (random < 0.5) {
